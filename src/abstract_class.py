b6f2b0eface338fa0199be970c371a52ebffb68f
ElnaraGasanova <elnaraskypro@gmail.com>
2023-12-25 14:29:45 +0300

    Создан файл для ввода API ключа. Создан файл с функциями для взаимодействия с пользователем через консоль.

--- src/abstract_class.py
+++ src/abstract_class.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 
 class JobBoard(ABC):
-    """ Абстрактный класс для работы с API сайтов с вакансиями."""
+    """ Абстрактный класс для работы по API с вакансиями."""
     @abstractmethod
     def get_vacancies(self, keyword: str):
         """ Функция подключения к сайту по API и получения списка вакансий."""
